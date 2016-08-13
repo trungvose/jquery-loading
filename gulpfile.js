12f946b17a92df54b02244878d75fbc387fa49e5
Vo Tuan Trung <trungk18@gmail.com>
2016-08-13 13:06:25 +0800

    Update bower, gulp

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+﻿//Include gulp
+var gulp = require('gulp');
+//Include plugin
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+//Concatenate JS file
+gulp.task('scripts', function () {
+    return gulp.src('js/*.js')
+        .pipe(concat('main.js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('js'));
+});
+
+//Default task
+gulp.task('default', ['scripts'])
