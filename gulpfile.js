c51b6671ee7b51528fdf9b0f0699563ad6c95e3e
Vo Tuan Trung <trungk18@gmail.com>
2016-08-14 13:51:13 +0800

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 ﻿//Include gulp
 var gulp = require('gulp');
+
 //Include plugin
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var sass = require('gulp-ruby-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
-//Concatenate JS file
+//Concatenate and minify JS
 gulp.task('scripts', function () {
     return gulp.src('js/*.js')
         .pipe(concat('main.js'))
@@ -14,5 +17,19 @@ gulp.task('scripts', function () {
         .pipe(gulp.dest('js'));
 });
 
+//SASS 
+gulp.task('sass', function () {
+    sass('scss/jquery-loading.scss', { sourcemap: true })
+        .on('error', sass.logError)
+        // for inline sourcemaps
+        .pipe(sourcemaps.write())
+        // for file sourcemaps
+        .pipe(sourcemaps.write('maps', {
+            includeContent: false,
+            sourceRoot: 'source'
+        }))
+        .pipe(gulp.dest('css'))
+});
+
 //Default task
-gulp.task('default', ['scripts'])
+gulp.task('default', ['scripts', 'sass'])
